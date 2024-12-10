c9036aa2a9d500198d7947472f956b1db9e4e5b7
kimjeffsj <kim.jeffsj@gmail.com>
2024-12-09 20:55:51 -0800

    Update shiftTradeApi.ts

--- src/features/shift-trade/api/shiftTradeApi.ts
+++ src/features/shift-trade/api/shiftTradeApi.ts
@@ -6,13 +6,17 @@ import {
   ShiftTradeResponse,
 } from "../types/shift-trade.type";
 
-const API_URL = import.meta.env.BASE_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const shiftTradeApi = {
-  // Get shift trade list
+  // Get trade requests
   getTradeRequests: async (
     token: string,
-    params?: { status?: string; type?: string }
+    params?: {
+      status?: string;
+      type?: string;
+      search?: string;
+    }
   ) => {
     const response = await axios.get<ShiftTradeRequest[]>(`${API_URL}/trades`, {
       headers: { Authorization: `Bearer ${token}` },
@@ -21,7 +25,18 @@ export const shiftTradeApi = {
     return response.data;
   },
 
-  // Create Shift Trade
+  // Get a trade request
+  getTradeRequest: async (token: string, id: number) => {
+    const response = await axios.get<ShiftTradeRequest>(
+      `${API_URL}/trades/${id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    return response.data;
+  },
+
+  // Create a new trade request
   createTradeRequest: async (token: string, data: CreateTradeRequest) => {
     const response = await axios.post<ShiftTradeRequest>(
       `${API_URL}/trades`,
@@ -33,8 +48,8 @@ export const shiftTradeApi = {
     return response.data;
   },
 
-  // Response for the request
-  respondToTrade: async (
+  // Response to trade request
+  createTradeResponse: async (
     token: string,
     tradeId: number,
     data: CreateTradeResponse
@@ -49,19 +64,43 @@ export const shiftTradeApi = {
     return response.data;
   },
 
-  // Accept trade
-  approveTradeResponse: async (
+  // Response Accepted | rejected
+  updateResponseStatus: async (
     token: string,
     tradeId: number,
-    responseId: number
+    responseId: number,
+    status: "ACCEPTED" | "REJECTED"
   ) => {
-    const response = await axios.post(
-      `${API_URL}/trades/${tradeId}/responses/${responseId}/approve`,
-      {},
+    const response = await axios.patch<ShiftTradeResponse>(
+      `${API_URL}/trades/${tradeId}/responses/${responseId}/status`,
+      { status },
       {
         headers: { Authorization: `Bearer ${token}` },
       }
     );
     return response.data;
+  },
+
+  // Cancel Trade request
+  cancelTradeRequest: async (token: string, tradeId: number) => {
+    await axios.patch(
+      `${API_URL}/trades/${tradeId}/cancel`,
+      {},
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+  },
+
+  setupInterceptors: () => {
+    axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status === 401) {
+          window.location.href = "/";
+        }
+        return Promise.reject(error);
+      }
+    );
   },
 };
